ff0c8ab2cac8687f91d11f5db8d17101a0535a71
Aditya Sutar <aditya1.sutar@outlook.com>
2025-04-18 15:44:09 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("hello world");
+  const message = "hello world";
 
   const handleClick = () => {
     alert("button clicked");
-  }
+  };
 
   return (
     <>
